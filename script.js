61e32a0e713db1a6cf8d09266afe96093d7e5694
Ygor Queiroz <ygor.silva@eaportal.org>
2024-04-04 22:23:37 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 let conta = 0
+let pessoas = 0
+let porcentagem = 0
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
 
-const containput = document.querySelector("#conta")
-containput.addEventListener("input", recebervalorconta)
-
-function recebervalorconta(event) {
-    conta = number(evento.target.value)
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
 }
 
-const pessoasinput = document.querySelector(#"pessoas")
-pessoasinput.addEventListener("input", receberquantidadePessoas)
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
 
-function receberquantidadePessoas(evento) {
+function receberQuantidadePessoas(evento) {
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+
     if(evento.target.value == "0") {
-        const paragrafoerro = document.querySelector(".pessoas #erro")
-        paragrafoerro.style.display = "block"
-
-        const diverro = document.querySelector(".pessoas .input-box")
-        diverro.setAttribute("id", "erro-div")
-    }else {
-        paragrafoerro.style.display = "none"
-        diverro.setAttribute("id", "")
-        pessoas= Number(evento.target.value)
+        paragrafoErro.style.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    } else {
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
     }
+}
 
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPOrcentagem)
+})
+
+function receberPOrcentagem(evento) {
+    porcentagem = Number(evento.target.value) / 100
+    console.log(porcentagem)
+}
